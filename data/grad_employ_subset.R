89de76e08aa96d7f49a5aceaee8909e731e12fd2
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2024-09-18 18:59:50 -0400

    add canadian grad income dataset

---
+++ data/grad_employ_subset.R
@@ -0,0 +1,13 @@
+delayedAssign("grad_employ_subset", local({
+  if (requireNamespace("epiprocess", quietly = TRUE)) {
+    d <- as.Date("2024-09-18")
+    epiprocess::as_epi_df(
+      epidatasets:::grad_employ_subset_dt,
+      as_of = d,
+      additional_metadata = list(other_keys = c("age_group", "edu_qual"))
+    )
+  } else {
+    warning("Since the package `epiprocess` is not installed, this object will be loaded as a tibble (class `tbl_df`)")
+    epidatasets:::grad_employ_subset_dt
+  }
+}))

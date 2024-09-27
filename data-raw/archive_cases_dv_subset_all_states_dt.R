c129398b0e5ad9ceac075909f6dbfcd540510b05
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2024-09-27 17:21:07 -0400

    remove territories from all_states archive to match epipredict original

--- data-raw/archive_cases_dv_subset_all_states_dt.R
+++ data-raw/archive_cases_dv_subset_all_states_dt.R
@@ -14,6 +14,8 @@ dv_subset <- pub_covidcast(
   issues = epirange(20200601, 20211201)
 ) %>%
   select(geo_value, time_value, version = issue, percent_cli = value) %>%
+  # Drop DC and territories.
+  filter(!(geo_value %in% c("as", "gu", "dc", "mp", "pr", "vi"))) %>%
   # We're using compactify=FALSE here and below to avoid some testthat test
   # failures on tests that were based on a non-compactified version.
   as_epi_archive(compactify = FALSE)
@@ -28,6 +30,7 @@ case_rate_subset <- pub_covidcast(
   issues = epirange(20200601, 20211201)
 ) %>%
   select(geo_value, time_value, version = issue, case_rate_7d_av = value) %>%
+  filter(!(geo_value %in% c("as", "gu", "dc", "mp", "pr", "vi"))) %>%
   as_epi_archive(compactify = FALSE)
 
 # Use `epiprocess::epix_merge` to avoid having to reimplement `sync`ing
